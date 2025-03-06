cdc89c804d70e199a9fa654a1627dbfae850c2b0
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-03-06 19:39:54 +0700

    init

---
+++ src/GamePanel.java
@@ -0,0 +1,54 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JPanel;
+
+
+public class GamePanel extends JPanel{
+    GamePanel(){
+
+    }
+
+    public void startGame(){
+
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void checkApple(){
+
+    }
+
+    public void checkCollision(){
+
+    }
+
+    public void gameOver(Graphics g){
+
+    }
+
+
+    public void actionPerformed(ActionEvent e){
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
+}
